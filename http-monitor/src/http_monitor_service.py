96d6e86e513e0ea78aafb6864c6bb6e22990f3d8
Ali <aliasad059>
2023-01-20 20:15:27 +0330

    feat: endpoints desired logic implemented

--- http-monitor/src/http_monitor_service.py
+++ http-monitor/src/http_monitor_service.py
@@ -1,3 +1,80 @@
+from http_monitor_db import dbManager
+
 class httpMonitorService:
     def __init__(self):
         pass
+
+    def authenticate_user(self, username, password):
+        # authenticate user with jwt token
+
+        # check if user exists
+        if not dbManager.user_exists(username):
+            return False
+
+        # check if password is correct
+        if not dbManager.check_password(username, password):
+            return False
+        
+        return True
+        
+    def create_user(self, username, password):
+        # create user
+        if dbManager.user_exists(username):
+            return False
+        if dbManager.create_user(username, password):
+            return True
+        return False
+    
+    def create_url(self, user_id, url, threshold):
+        # create a new url for the authenticated user (at most 20 urls per user)
+        if dbManager.get_urls_count(user_id) >= 20:
+            return False
+        if dbManager.create_url(user_id, url, threshold):
+            return True
+        return False
+    
+    def get_urls(self, user_id):
+        # get all urls for the authenticated user
+        urls = []
+        for url in dbManager.get_urls(user_id):
+            urls.append({
+                "url_id": url[0],
+                "url": url[3],
+                "threshold": url[5],
+                "method": url[4],
+                "created_at": url[1]
+            })
+        if len(urls) == 0:
+            return False
+        return urls
+    
+    def get_url_status(self, user_id, url_id):
+        # get the status of a url for the authenticated user
+        if not dbManager.url_exists(user_id, url_id):
+            return False
+        
+        failed_requests_count = dbManager.get_failed_requests_count(user_id, url_id)
+        threshold = dbManager.get_threshold(user_id, url_id)
+        total_requests_count = dbManager.get_requests_count(user_id, url_id)
+
+        return {
+            "failed_requests_count": failed_requests_count,
+            "threshold": threshold,
+            "total_requests_count": total_requests_count
+        }
+        
+    def get_alerts(self, user_id):
+        # get all alerts for the authenticated user
+        # check all the urls for the user that have failed requests count >= threshold
+        urls = dbManager.get_urls(user_id)
+        alerts = []
+        for url in urls:
+            failed_requests_count = dbManager.get_failed_requests_count(user_id, url["url_id"])
+            if failed_requests_count >= url["threshold"]:
+                alerts.append({
+                    "url": url["url"],
+                    "failed_requests_count": failed_requests_count
+                })
+        if len(alerts) == 0:
+            return False
+        return alerts
--- http-monitor/src/main.py
+++ http-monitor/src/main.py
@@ -2,51 +2,72 @@ import os
 
 import uvicorn
 from dotenv import load_dotenv
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, Depends
 from prometheus_fastapi_instrumentator import Instrumentator
 from uvicorn.config import LOGGING_CONFIG
 
-from http_monitor_db import dbManager
 from http_monitor_service import httpMonitorService
+from .auth.auth_handler import signJWT, decodeJWT
+from .auth.auth_bearer import JWTBearer
 
 load_dotenv()
 
 http_monitor_service = httpMonitorService()
-db_manager = dbManager()
 
 app = FastAPI()
 Instrumentator().instrument(app).expose(app)
 
+
 @app.post("/api/users/login")
 async def login(username: str = Form(...), password: str = Form(...)):
     # authenticate user with jwt token
-    return
+    if http_monitor_service.authenticate_user(username, password):
+        signed_jwt = signJWT(username)
+        return signed_jwt
+    return {"message": "Invalid credentials"}
 
 @app.post("/api/users")
 async def create_user(username: str = Form(...), password: str = Form(...)):
     # create user
-    return
+    if http_monitor_service.create_user(username, password):
+        signed_jwt = signJWT(username)
+        return signed_jwt
+    return {"message": "User not created"}
 
 @app.post("/api/urls")
-async def create_url(url: str = Form(...), threshold: int = Form(...)):
+async def create_url(url: str = Form(...), threshold: int = Form(...), dependencies=[Depends(JWTBearer())]):
     # create a new url for the authenticated user (at most 20 urls per user)
-    return
+    current_user = decodeJWT(dependencies[0])
+    if http_monitor_service.create_url(current_user["user_id"], url, threshold):
+        return {"message": "URL created"}
+    return {"message": "URL not created"}
 
-@app.get("/api/urls")
+@app.get("/api/urls", dependencies=[Depends(JWTBearer())])
 async def get_urls():
     # get all urls for the authenticated user
-    return
-
-@app.get("/api/urls/{url_id}")
+    current_user = decodeJWT(dependencies[0])
+    urls = http_monitor_service.get_urls(current_user["user_id"])
+    if urls:
+        return urls
+    return {"message": "No URLs found"}
+    
+@app.get("/api/urls/{url_id}", dependencies=[Depends(JWTBearer())])
 async def get_url_status(url_id: str):
     # get the status of a url for the authenticated user
-    return
+    current_user = decodeJWT(dependencies[0])
+    url_status = http_monitor_service.get_url_status(current_user["user_id"], url_id)
+    if url_status:
+        return url_status
+    return {"message": "URL not found"}
 
-@app.get("/api/alerts")
+@app.get("/api/alerts", dependencies=[Depends(JWTBearer())])
 async def get_alerts():
     # get all alerts for the authenticated user
-    return
-
+    current_user = decodeJWT(dependencies[0])
+    alerts = http_monitor_service.get_alerts(current_user["user_id"])
+    if alerts:
+        return alerts
+    return {"message": "No alerts found"}
 
 def run():
     LOGGING_CONFIG["formatters"]["default"]["fmt"] = "%(asctime)s [%(name)s] %(levelprefix)s %(message)s"
